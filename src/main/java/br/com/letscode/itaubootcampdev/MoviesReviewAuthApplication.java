449cf07e7b0cc091fef64e7db18133556cbad8b3
gabrieloswaldo <gabriel.oswaldo99@gmail.com>
2022-06-29 20:46:48 -0300

    Init project

---
+++ src/main/java/br/com/letscode/itaubootcampdev/MoviesReviewAuthApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.itaubootcampdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesReviewAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesReviewAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/letscode/itaubootcampdev/MoviesReviewAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.itaubootcampdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesReviewAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
